c8a9578b6883723a730204125d31bb7456e42449
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 14:32:03 -0400

    fix: seperate subnet module

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
 output "virtual_network_name" {
   value = azurerm_subnet.instance.virtual_network_name
 }
-output "virtual_subnet_name" {
-  value = azurerm_subnet.instance.name
-}
-output "virtual_subnet_id" {
-  value = azurerm_subnet.instance.id
-}
-output "virtual_subnet_id_ag" {
-  value = azurerm_subnet.ag.id
-}
