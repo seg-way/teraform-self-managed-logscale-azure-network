675eedfac89fee4cb0c398b46682fec6e881fd67
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 14:31:44 -0400

    fix!: seperate subnet module

--- main.tf
+++ main.tf
@@ -1,31 +1,9 @@
 resource "azurerm_virtual_network" "instance" {
   address_space       = [var.network]
   location            = var.location
-  name                = "vnet"
+  name                = var.name
   resource_group_name = var.resource_group
 
   tags = var.tags
 }
 
-resource "azurerm_subnet" "instance" {
-  address_prefixes                          = [var.subnet]
-  name                                      = "k8s"
-  resource_group_name                       = var.resource_group
-  virtual_network_name                      = azurerm_virtual_network.instance.name
-  private_endpoint_network_policies_enabled = true
-  service_endpoints = [
-    "Microsoft.EventHub",
-    "Microsoft.ContainerRegistry",
-    "Microsoft.KeyVault",
-    "Microsoft.Storage"
-  ]
-}
-resource "azurerm_subnet" "ag" {
-  address_prefixes                          = [var.subnet_ag]
-  name                                      = "ag"
-  resource_group_name                       = var.resource_group
-  virtual_network_name                      = azurerm_virtual_network.instance.name
-  private_endpoint_network_policies_enabled = true
-
-
-}
--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "resource_group" {
   type        = string
   description = "(optional) describe your variable"
 }
+variable "name" {
+  type        = string
+  description = "(optional) describe your variable"
+}
 variable "location" {
   type        = string
   description = "(optional) describe your variable"
@@ -12,14 +16,6 @@ variable "tags" {
 }
 
 variable "network" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-variable "subnet" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-variable "subnet_ag" {
   type        = string
   description = "(optional) describe your variable"
 }
